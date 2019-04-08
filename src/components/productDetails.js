3b05f58018147d2ec988d574b981e51734f9d5d1
Marius Guei <marius.g.guei@gmail.com>
2019-04-09 01:21:53 +0200

    commented out productdetails and productImg

--- src/components/productDetails.js
+++ src/components/productDetails.js
@@ -1,14 +1,14 @@
-import React from 'react';
-
-
-const ProductDetails = (props) => {
-  return (
-    <div className="">
-      <h3>{props.title}</h3>
-      <p>{props.brand}</p>
-      <p>{props.size}</p>
-      <p>{props.price}</p>
-    </div>
-  )
-};
-export default ProductDetails;
+// import React from 'react';
+//
+//
+// const ProductDetails = (props) => {
+//   return (
+//     <div className="">
+//       <h3>{props.title}</h3>
+//       <p>{props.brand}</p>
+//       <p>{props.size}</p>
+//       <p>{props.price}</p>
+//     </div>
+//   )
+// };
+// export default ProductDetails;
--- src/components/productImg.js
+++ src/components/productImg.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
-
-const ProductImg = (props) => {
-  return (
-    <div className="person">
-      <img src={props.img} alt={props.description}/>
-    </div>
-  )
-};
-export default ProductImg;
+// import React from 'react';
+//
+//
+// const ProductImg = (props) => {
+//   return (
+//     <div className="person">
+//       <img src={props.img} alt={props.description}/>
+//     </div>
+//   )
+// };
+// export default ProductImg;
